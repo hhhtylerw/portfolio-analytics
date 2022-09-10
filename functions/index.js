54289b19cd3dd85a0175464f191f8009f6e7007e
hhhtylerw <hhhtylerw@gmail.com>
2022-09-09 23:56:29 -0400

    cors update

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const { firestore } = require("firebase-admin");
 const functions = require("firebase-functions");
+const cors = require('cors')({origin: true});
 
 db = new firestore.Firestore();
 
@@ -9,36 +10,42 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 });
 
 exports.analytics = functions.https.onRequest((request, response) => {
-    const unixtime = new Date().getTime().toString();
-    const stringtime = new Date().toLocaleString();
-
-    db.collection("visits").doc(unixtime).set({
-      "time": stringtime,
-        "ip": request.ip,
-        "user-agent": request.headers["user-agent"],
-    })
-
-    response.send("Thanks for taking a look ;)");
+    cors(request, response, () => {
+        const unixtime = new Date().getTime().toString();
+        const stringtime = new Date().toLocaleString();
+    
+        db.collection("visits").doc(unixtime).set({
+          "time": stringtime,
+            "ip": request.ip,
+            "user-agent": request.headers["user-agent"],
+        })
+    
+        response.send("Thanks for taking a look ;)");
+    });
 });
 
 exports.totalVisits = functions.https.onRequest((request, response) => {
-  db.collection("visits").get().then((querySnapshot) => {
-      response.send(querySnapshot.size.toString());
-  });
+    cors(request, response, () => {
+        db.collection("visits").get().then((querySnapshot) => {
+            response.send(querySnapshot.size.toString());
+        });
+    });    
 });
 
 exports.uniqueVisits = functions.https.onRequest((request, response) => {
-    db.collection("visits").get().then((querySnapshot) => {
-        var count = 0;
-        const used = new Set();
-
-        querySnapshot.forEach((doc) => {
-            if (!used.has(doc.data().ip)) {
-                used.add(doc.data().ip);
-                count++;
-            }
+    cors(request, response, () => {
+        db.collection("visits").get().then((querySnapshot) => {
+            var count = 0;
+            const used = new Set();
+    
+            querySnapshot.forEach((doc) => {
+                if (!used.has(doc.data().ip)) {
+                    used.add(doc.data().ip);
+                    count++;
+                }
+            });
+    
+            response.send(count.toString());
         });
-
-        response.send(count.toString());
     });
 });
