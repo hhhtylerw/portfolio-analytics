befefb8ee1a8fc50b2c8fb245112552f8d7754fe
hhhtylerw <hhhtylerw@gmail.com>
2022-09-09 23:28:48 -0400

    uniqueVisits and totalVisits

--- functions/index.js
+++ functions/index.js
@@ -4,19 +4,41 @@ const functions = require("firebase-functions");
 db = new firestore.Firestore();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+  // functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
 
 exports.analytics = functions.https.onRequest((request, response) => {
-    const time = new Date().getTime().toString();
+    const unixtime = new Date().getTime().toString();
+    const stringtime = new Date().toLocaleString();
 
-    db.collection("analytics").doc(time).set({
-        "hello": "world1",
-        "ip": request.ip
+    db.collection("visits").doc(unixtime).set({
+      "time": stringtime,
+        "ip": request.ip,
+        "user-agent": request.headers["user-agent"],
     })
 
     response.send("Thanks for taking a look ;)");
 });
 
+exports.totalVisits = functions.https.onRequest((request, response) => {
+  db.collection("visits").get().then((querySnapshot) => {
+      response.send(querySnapshot.size.toString());
+  });
+});
 
+exports.uniqueVisits = functions.https.onRequest((request, response) => {
+    db.collection("visits").get().then((querySnapshot) => {
+        var count = 0;
+        const used = new Set();
+
+        querySnapshot.forEach((doc) => {
+            if (!used.has(doc.data().ip)) {
+                used.add(doc.data().ip);
+                count++;
+            }
+        });
+
+        response.send(count.toString());
+    });
+});
